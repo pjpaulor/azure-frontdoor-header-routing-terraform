64cf566104fa752b0c810be3bbbacd9cb28a6718
Paulo Rodrigues Jr <p.rodrigues-junior@bjss.com>
2024-04-10 15:42:57 +0100

    add storage accounts

--- main.tf
+++ main.tf
@@ -4,18 +4,34 @@ provider "azurerm" {
 }
 
 # Generate random names
-resource "random_pet" "random_names" {
+resource "random_pet" "name" {
   length    = 2
   separator = "-"
 }
 
 # Generate hash codes for resource names
 locals {
-  hash_code = substr(sha1(random_pet.random_names.*.id), 0, 5)
+  hash_code = substr(sha1(random_pet.name.id), 0, 5)
 }
 
 # Create resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg--${local.hash_code}"
-  location = "West Europe"
+  name     = "rg-headerrouting-${local.hash_code}"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "frontends" {
+  count             = length(["blue", "green"])
+  name              = "st-${element(["frontendblue", "frontendgreen"], count.index % 2)}-${local.hash_code}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location          = var.location
+  account_tier      = "Standard"
+  account_replication_type = "LRS"
+  
+  # Enable blob public access
+  allow_nested_items_to_be_public = true
+
+  tags = {
+    environment = element(["blue", "green"], count.index % 2)
+  }
 }
